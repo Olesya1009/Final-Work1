606892752d9743041927be6d3c5ae2af5f62422c
Olesya <Olesyaholod@mail.ru>
2022-09-21 21:12:46 +0300

    Контрольная

---
+++ FinalTask/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+//длина которых меньше, либо равна 3 символам. 
+//Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+//При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+string[] array = new string[6] { "Hello", "2", "world", "386", "cat", "Russia" };
+string[] select = new string[array.Length];
+
+Getarray(array, select);
+PrintArray(array);
+
+PrintArray(select);
+
+void Getarray(string[] array, string[] select)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            select[count] = array[i];
+            count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
